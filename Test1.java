ed13827940ed4cdcd932a5497ec7809cc39336c3
ramazandogaan <ramazandogan5514@gmail.com>
2025-09-16 21:10:36 +0300

    Update Test1

--- Test1.java
+++ Test1.java
@@ -2,6 +2,6 @@ package Banka;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("ilk testimi yaptım");
+        System.out.println("düzeltildi");
     }
 }
